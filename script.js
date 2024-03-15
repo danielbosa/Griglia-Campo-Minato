c0b9b2b281d2aea0ead100436d0cb58d48a4b422
danielbosa <danielbosa95@gmail.com>
2024-03-15 15:43:14 +0100

    Get ready: basic structure in html (on going)

---
+++ script.js
@@ -0,0 +1,12 @@
+//prendo il bottone dall'html
+const btn = document.getElementById('btn-start');
+
+//aggiungo event listener al bottone
+btn.addEventListener('click',function(){
+
+});
+
+//ciclo for per generare le celle
+for (i = 0; i < 100; i++){
+    
+};
